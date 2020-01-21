1b9ac4a6f64229ffd48f3f9cefe6ea5b9a74325e
supre7me <907331074@qq.com>
2020-01-21 09:02:42 +0800

    添加基于fastjson封装的json转换工具类--单元测试

---
+++ src/test/java/com/supreme7/springboot_springmvc/util/FastJsonUtilsTest.java
@@ -0,0 +1,30 @@
+package com.supreme7.springboot_springmvc.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author: supreme7
+ * Date: Created in 8:58 20/01/21
+ * Description:
+ */
+public class FastJsonUtilsTest {
+
+    @Test
+    public void getJsonToBean() {
+    }
+
+    @Test
+    public void getBeanToJson() {
+    }
+
+    @Test
+    public void getJsonToList() {
+    }
+
+    @Test
+    public void getJsonToListMap() {
+    }
+}
